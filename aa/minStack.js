6070b8b4de8c58a6a14d220f8666ca5d16fa09b4
Kelby Lu <kelby.lu28@gmail.com>
2018-09-01 13:27:01 -0400

    Set up tests and question for minStack

---
+++ aa/minStack.js
@@ -0,0 +1,7 @@
+// Create a stack that has the push, pop, peek functionality, as well as a min function, that returns the lowest integer in the stack
+
+class minStack {
+
+}
+
+module.exports = minStack;
---
+++ aa/spec/aa.spec.js
@@ -0,0 +1,17 @@
+const MinStack = require('../minStack');
+
+describe("minStack", () => {
+  let stackOne = new MinStack();
+
+  test("pop should remove the integer at the top of the stack", () => {
+
+  });
+
+  test("peek should return the value of the integer at the top of the stack", () => {
+
+  });
+
+  test("push should add the given integer to the top of the stack", () => {
+
+  });
+});
