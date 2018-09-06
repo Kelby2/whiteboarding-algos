96ae7fa3fff8d286596dab3b60a16a878d64ed37
Kelby Lu <kelby.lu28@gmail.com>
2018-09-06 12:41:45 -0400

    Complete remove dups for linked lists

--- ctci/linked_lists/2.1_removeDups.js
+++ ctci/linked_lists/2.1_removeDups.js
@@ -1,8 +1,26 @@
 // Write code to remove duplicates from an unsorted linked list.
 // How would you solve this problem if a temporary buffer is not allowed?
 
-function removeDups(head) {
+//***This does not remove all duplicate occurences. Just returns a LL with no dupes***
 
+function removeDups(headNode) {
+
+  const seen = {};
+
+  let currNode = headNode;
+
+  while (currNode !== null) {
+    seen[currNode.val] = true;
+
+    let nextNode = currNode.next;
+    while (nextNode !== null && seen[nextNode.val]) {
+      nextNode = nextNode.next;
+    }
+
+    currNode.next = nextNode;
+    currNode = nextNode;
+  }
+  return headNode;
 }
 
 module.exports = removeDups;
