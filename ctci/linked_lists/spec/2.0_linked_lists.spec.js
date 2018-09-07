8c127d4ce8d46c4915aaa5da6816a76464925512
Kelby Lu <kelby.lu28@gmail.com>
2018-09-06 20:28:42 -0400

    Update tests

--- ctci/linked_lists/spec/2.0_linked_lists.spec.js
+++ ctci/linked_lists/spec/2.0_linked_lists.spec.js
@@ -8,7 +8,7 @@ const palindrome = require('../2.6_palindrome');
 const intersection = require('../2.7_intersection');
 const loopDetection = require('../2.8_loopDetection');
 
-const nodes = [6,5,3,3,5,1,2,3,8,9].map(num => new Node(num));
+const nodes = [6,5,3,3,5,1,2,3,9,8].map(num => new Node(num));
 for (let i = 0; i < nodes.length - 1; i++) {
   nodes[i].next = nodes[i + 1];
 }
@@ -29,18 +29,19 @@ describe("removeDups", () => {
   afterAll(() => {
     resetNodes();
   });
+
   let head = removeDups(nodes[0]);
   let head2 = removeDups(dupNodes[0]);
 
   test("should remove duplicates", () => {
-    expect(head.remainingLinks()).toBe("[6,5,3,1,2,8,9]");
+    expect(head.remainingLinks()).toBe("[6,5,3,1,2,9,8]");
     expect(head2.remainingLinks()).toBe("[5]");
   });
 
 });
 
 describe("returnKthToLast", () => {
-  //current list is [6->5->3->3->5->1->5->2->3->8->9]
+  //current list is [6->5->3->3->5->1->2->3->8->9]
   let head = nodes[0];
 
   test("should return the kth to last element in the linked list", () => {
@@ -52,17 +53,37 @@ describe("returnKthToLast", () => {
 });
 
 describe("deleteMiddleNode", () => {
-  //current list is [6->5->3->3->5->1->5->2->3->8->9]
+  //current list is [6->5->3->3->5->1->2->3->8->9]
+
+  afterAll(() => {
+    resetNodes();
+  });
 
   test("", () => {
     deleteMiddleNode(nodes[5]);
-    expect(nodes[0].remainingLinks()).toBe("[6,5,3,3,5,2,3,8,9]");
+    expect(nodes[0].remainingLinks()).toBe("[6,5,3,3,5,2,3,9,8]");
   });
 });
 
 describe("partition", () => {
-  test("", () => {
+  //current list is [6->5->3->3->5->2->2->3->8->9]
 
+  afterEach(() => {
+    resetNodes();
+  });
+
+  test("should partition the nodes based on the values and separator", () => {
+    let partitionedList = partition(nodes[0], 5);
+    expect(partitionedList.remainingLinks()).toBe("[3,3,2,2,3,6,5,5,9,8]");
+  });
+
+  test("should return the same list if there is only one side", () => {
+    let partitionedList2 = partition(nodes[0], 2);
+    expect(partitionedList2.remainingLinks()).toBe("[6,5,3,3,5,2,2,3,9,8]");
+
+    console.log('hi');
+    let partitionedList3 = partition(nodes[0], 9);
+    expect(partitionedList3.remainingLinks()).toBe("[6,5,3,3,5,2,2,3,8,9]");
   });
 });
 
