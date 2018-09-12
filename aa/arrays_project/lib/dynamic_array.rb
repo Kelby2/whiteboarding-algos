d1f2ff8e27a5c95285a7fabe4358af98847226c3
Kelby Lu <kelby.lu28@gmail.com>
2018-09-12 16:42:40 -0400

    Complete dynamic array

--- aa/arrays_project/lib/dynamic_array.rb
+++ aa/arrays_project/lib/dynamic_array.rb
@@ -4,41 +4,63 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @store = []
+    @length = 0
+    @capacity = 8
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    check_index
+    @store.pop
+    @length -= 1
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if @length == @capacity
+    @store.push(val)
+    @length += 1
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    check_index
+    @store.shift
+    @length -= 1
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if @length == @capacity
+    @store.unshift(val)
+    @length += 1
   end
 
   protected
   attr_accessor :capacity, :store
   attr_writer :length
 
-  def check_index(index)
+  def check_index(index = 0)
+    if @length == 0 || index >= @length
+      raise "index out of bounds"
+    end
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    @capacity *= 2
   end
 end
