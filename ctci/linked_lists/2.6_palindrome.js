a0536359c931e823801f71a70370dc75c2986144
Kelby Lu <kelby.lu28@gmail.com>
2018-09-12 13:39:24 -0400

    Complete palindrome with two other approaches (using stack and constant space)

--- ctci/linked_lists/2.6_palindrome.js
+++ ctci/linked_lists/2.6_palindrome.js
@@ -31,7 +31,90 @@ function palindrome(node) {
   }
 
   return JSON.stringify(firstSection) === JSON.stringify(secondSection.reverse());
-
 }
 
-module.exports = palindrome;
+function palindromeStackApproach(node) {
+  const valueStack = [];
+
+  let current = node;
+  while (current !== null) {
+    valueStack.push(current.val);
+    current = current.next;
+  }
+
+  current = node;
+  for (let i = valueStack.length - 1; i >= 0; i--) {
+    if (current.val !== valueStack[i]) {
+      return false;
+    }
+    current = current.next;
+  }
+
+  return true;
+}
+
+function palindromeConstantSpace(node) {
+  /*
+    iterate through the list, find the halfway point, break off and reverse the latter half of the linked list
+    compare the two linked lists
+    reverse and re-append the latter half back to the original list
+  */
+  let slowPointer = node;
+  let fastPointer = node;
+  let firstHalf = node;
+  let midpoint;
+  let secondHalf;
+
+  while (fastPointer !== null && fastPointer.next !== null) {
+    behindSlowPointer = slowPointer;
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+  }
+
+  if (fastPointer !== null) {
+    midpoint = slowPointer;
+    slowPointer = slowPointer.next;
+  }
+
+  behindSlowPointer.next = null;
+  slowPointer = reverse(slowPointer);
+
+  let res = compareLists(firstHalf, slowPointer);
+  slowPointer = reverse(slowPointer);
+
+  if (midpoint) {
+    midpoint.next = slowPointer;
+    behindSlowPointer.next = midpoint;
+  } else {
+    behindSlowPointer.next = slowPointer;
+  }
+
+  return res;
+}
+
+function compareLists(node1, node2) {
+  while (node1 !== null && node2 !== null) {
+    if (node1.val !== node2.val) {
+      return false;
+    }
+    node1 = node1.next;
+    node2 = node2.next;
+  }
+
+  return node1 === node2;
+}
+
+function reverse(node) {
+  let prev = null;
+  let next;
+  while (node !== null) {
+    next = node.next;
+    node.next = prev;
+    prev = node;
+    node = next;
+  }
+
+  return prev;
+}
+
+module.exports = palindromeConstantSpace;
