88cb0584acd8eb6542487b610887388f9de638cd
Kelby Lu <kelby.lu28@gmail.com>
2018-09-12 23:58:34 -0400

    Add comments to methods

--- aa/arrays_project/lib/dynamic_array.rb
+++ aa/arrays_project/lib/dynamic_array.rb
@@ -24,35 +24,51 @@ class DynamicArray
   # O(1)
   def pop
     check_index
+    popped = self[@length - 1]
     @length -= 1
+    # reset that space in memory to nil
+    self[@length] = nil
+
+    # pop returns the element that was popped
+    popped
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
     resize! if @length == @capacity
-    @store[@length] = val
+    self[@length] = val
     @length += 1
+
+    # return a copy of the current store (protect against tampering)
+    @store.dup
   end
 
   # O(n): has to shift over all the elements.
   def shift
     check_index
+    shifted = self[0]
     @length.times do |n|
-      @store[n] = @store[n + 1]
+      self[n] = @store[n + 1]
     end
-
     @length -= 1
+
+    shifted
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if @length == @capacity
+    # @length..1, copying from the back of the array to the front
     @length.downto(1) do |n|
-      @store[n] = @store[n - 1]
+      self[n] = self[n - 1]
     end
 
-    @store[0] = val
+    self[0] = val
     @length += 1
+
+    # return a copy of the current store (protect against tampering)
+    @store.dup
   end
 
   protected
@@ -67,8 +83,10 @@ class DynamicArray
   def resize!
     @capacity *= 2
     new_store = StaticArray.new(@capacity)
+
+    # 0...length, copy the elements over to the new_store
     @length.times do |n|
-      new_store[n] = @store[n]
+      new_store[n] = self[n]
     end
 
     @store = new_store
