7a9391bbc2a499f2156adf873ff06c148ab42b7e
Kelby Lu <kelby.lu28@gmail.com>
2018-09-15 00:56:11 -0400

    Complete node

--- aa/binary_search_tree/lib/bst_node.rb
+++ aa/binary_search_tree/lib/bst_node.rb
@@ -1,4 +1,11 @@
 class BSTNode
+  attr_reader :value
+  attr_accessor :parent, :left, :right
+
   def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+    @parent = nil
   end
 end
