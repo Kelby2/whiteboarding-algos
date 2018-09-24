e49e03e7d2227647ff7b75319930f71c4d2fc773
Kelby Lu <kelby.lu28@gmail.com>
2018-09-24 14:24:28 -0400

    Comment for remainingLinks

--- ctci/linked_lists/lib/node.js
+++ ctci/linked_lists/lib/node.js
@@ -4,9 +4,10 @@ class Node {
     this.next = null;
   }
 
+  // used for test cases, since we can't compare objects, this will print out the remaining links following the node and check for equality between the strings
   remainingLinks() {
     const values = [];
-    let that = this;    
+    let that = this;
     while (that !== null) {
       values.push(that.val);
       that = that.next;
