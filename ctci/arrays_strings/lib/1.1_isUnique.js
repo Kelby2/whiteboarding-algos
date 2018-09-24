aee19e026d51f855cca2e5233c6de83f42bbb23f
Kelby Lu <kelby.lu28@gmail.com>
2018-09-24 14:16:38 -0400

    Move problem set and removed answers into lib folder

---
+++ ctci/arrays_strings/lib/1.1_isUnique.js
@@ -0,0 +1,16 @@
+// Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+// O(n) approach
+
+function isUnique(string) {
+
+}
+
+// Approach without using additional data structure
+
+function isUnique2(string) {
+
+}
+
+module.exports = isUnique;
+module.exports = isUnique2;
---
+++ ctci/arrays_strings/lib/1.4_palindromePermutation.js
@@ -0,0 +1,11 @@
+// Given a string, write a function to check if it is a permutation of a palin- drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.The palindrome does not need to be limited to just dictionary words.
+
+// Reworded: Given a string, write a function to check if one of it's permutations is a palindrome.
+
+// Assumptions: case-insensitive, ignore spaces
+
+function palinPerm(str) {
+  
+}
+
+module.exports = palinPerm;
